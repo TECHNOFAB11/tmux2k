1dfa877f18b3673999ac7141d388ad0790b85a55
Technofab <34860318+TECHNOFAB11@users.noreply.github.com>
2024-11-26 18:08:23 +0100

    Update current.sh

--- scripts/current.sh
+++ scripts/current.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$current_dir"/utils.sh
+
 start_icon=$(get_tmux_option "@tmux2k-start-icon" "session")
 
 main() {
+    current=""
     case $start_icon in
-    session) start_icon=" #S" ;;
-    window) start_icon=" #W" ;;
+    session) current=" #S" ;;
+    window) current=" #W" ;;
     esac
 
-    echo "$start_icon"
+    echo "$current"
 }
 
 main
